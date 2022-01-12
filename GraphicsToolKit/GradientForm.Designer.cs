6289653d999b9dd45e18a3072f3294d691dc132b
M.Amini <moortezaa.1378.ma@gmail.com>
2022-01-12 10:24:38 +0330

    Gradient Form

---
+++ GraphicsToolKit/GradientForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace GraphicsToolKit
+{
+    partial class GradientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.someThingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.someThingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1001, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // someThingToolStripMenuItem
+            // 
+            this.someThingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.someThingToolStripMenuItem.Name = "someThingToolStripMenuItem";
+            this.someThingToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.someThingToolStripMenuItem.Text = "Some Thing";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(213, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 100);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(213, 179);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 100);
+            this.panel2.TabIndex = 5;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel2_Paint);
+            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseClick);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(213, 285);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(776, 100);
+            this.panel3.TabIndex = 5;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel3_Paint);
+            this.panel3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel3_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(213, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 23);
+            this.label1.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 100);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(0, 179);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(207, 100);
+            this.listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(0, 285);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(207, 100);
+            this.listBox3.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 392);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem someThingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GraphicsToolKit/GradientForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsToolKit
+{
+    public partial class GradientForm : Form
+    {
+        Point ShowPoint = new Point(0, 0);
+        List<Point> Points1 { get; set; } = new List<Point>();
+        List<Point> Points2 { get; set; } = new List<Point>();
+        List<Point> Points3 { get; set; } = new List<Point>();
+        GraphicsPath Path1 { get; set; } = new GraphicsPath();
+        GraphicsPath Path2 { get; set; } = new GraphicsPath();
+        GraphicsPath Path3 { get; set; } = new GraphicsPath();
+        float Tention { set; get; } = 0.1f;
+        public GradientForm()
+        {
+            InitializeComponent();
+            listBox1.DisplayMember = nameof(PointShow.ToString);
+            listBox2.DisplayMember = nameof(PointShow.ToString);
+            listBox3.DisplayMember = nameof(PointShow.ToString);
+            float labelsWidth = panel1.Width / 255f;
+            label1.Width = (int)labelsWidth;
+            int left = label1.Left;
+
+            for (int i = 2; i <= 255; i++)
+            {
+                var label = new Label()
+                {
+                    Name = $"label{i}",
+                    Width = (int)labelsWidth,
+                    Left = (int)((float)left + labelsWidth * (float)(i - 1)),
+                    Top = label1.Top,
+                    BackColor = Color.Black,
+                    Height = label1.Height
+                };
+                Controls.Add(label);
+            }
+            ClearToolStripMenuItem_Click(new object(), new EventArgs());
+        }
+
+        private void ColorLabels()
+        {
+            float count = 0;
+            foreach (Control label in Controls)
+            {
+                if (label.GetType() == typeof(Label))
+                {
+                    count += 1;
+                    int r = 0;
+                    for (r = 0; r <= panel1.Height; r++)
+                    {
+                        if (Path1.IsOutlineVisible(count / 255f * panel1.Width, r, new Pen(Color.Red, 0.01f)))
+                            break;
+                    }
+                    r = (int)((float)r / (float)panel1.Height * 255f);
+                    int g = 0;
+                    for (g = 0; g <= panel2.Height; g++)
+                    {
+                        if (Path2.IsOutlineVisible(count / 255f * panel2.Width, g, new Pen(Color.Red, 0.01f)))
+                            break;
+                    }
+                    g = (int)((float)g / (float)panel2.Height * 255f);
+                    int b = 0;
+                    for (b = 0; b <= panel3.Height; b++)
+                    {
+                        if (Path3.IsOutlineVisible(count / 255f * panel3.Width, b, new Pen(Color.Red, 0.01f)))
+                            break;
+                    }
+                    b = (int)((float)b / (float)panel3.Height * 255f);
+                    CutForColor(ref r);
+                    CutForColor(ref g);
+                    CutForColor(ref b);
+                    label.BackColor = Color.FromArgb(r, g, b);
+                }
+            }
+        }
+
+        private static void CutForColor(ref int r)
+        {
+            r = r > 255 ? 255 : r < 0 ? r = 0 : r;
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Points1.Clear();
+            listBox1.Items.Clear();
+            Point point0 = new Point(0, 0);
+            Points1.Add(point0);
+            Points1.Add(new Point(panel1.Width, panel1.Height));
+            listBox1.Items.Add(new PointShow() { point = point0 });
+            listBox1.Items.Add(new PointShow() { point = new Point(panel1.Width, panel1.Height) });
+            Path1 = new GraphicsPath();
+            Path1.AddCurve(Points1.OrderBy(p => p.X).ToArray(), Tention);
+            Points2.Clear();
+            listBox2.Items.Clear();
+            Points2.Add(point0);
+            Points2.Add(new Point(panel2.Width, panel2.Height));
+            listBox2.Items.Add(new PointShow() { point = point0 });
+            listBox2.Items.Add(new PointShow() { point = new Point(panel2.Width, panel1.Height) });
+            Path2 = new GraphicsPath();
+            Path2.AddCurve(Points2.OrderBy(p => p.X).ToArray(), Tention);
+            Points3.Clear();
+            listBox3.Items.Clear();
+            Points3.Add(point0);
+            Points3.Add(new Point(panel3.Width, panel3.Height));
+            listBox3.Items.Add(new PointShow() { point = point0 });
+            listBox3.Items.Add(new PointShow() { point = new Point(panel3.Width, panel1.Height) });
+            Path3 = new GraphicsPath();
+            Path3.AddCurve(Points3.OrderBy(p => p.X).ToArray(), Tention);
+            ColorLabels();
+            panel1.Refresh();
+            panel2.Refresh();
+            panel3.Refresh();
+            listBox1.Refresh();
+            listBox2.Refresh();
+            listBox3.Refresh();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Draw the points.
+            foreach (Point point in Points1)
+                e.Graphics.FillEllipse(Brushes.Black,
+                    point.X - 3, point.Y - 3, 5, 5);
+            if (Points1.Count < 2) return;
+
+
+            // Draw the curve.
+            Path1 = new GraphicsPath();
+            Path1.AddCurve(Points1.OrderBy(p => p.X).ToArray(), Tention);
+            e.Graphics.DrawPath(Pens.Red, Path1);
+            e.Graphics.FillEllipse(Brushes.Blue, ShowPoint.X - 3, ShowPoint.Y - 3, 5, 5);
+        }
+
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                var thePoint = (PointShow)listBox1.SelectedItem;
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                Points1.Remove(thePoint.point);
+            }
+            Points1.Add(e.Location);
+            listBox1.Items.Add(new PointShow() { point = e.Location });
+            listBox1.Refresh();
+            panel1.Refresh();
+            ColorLabels();
+        }
+        private struct PointShow
+        {
+            public Point point;
+
+            override
+            public string ToString()
+            {
+                return $"X: {point.X} Y: {point.Y}";
+            }
+        }
+        private void Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Draw the points.
+            foreach (Point point in Points2)
+                e.Graphics.FillEllipse(Brushes.Black,
+                    point.X - 3, point.Y - 3, 5, 5);
+            if (Points2.Count < 2) return;
+
+
+            // Draw the curve.
+            Path2 = new GraphicsPath();
+            Path2.AddCurve(Points2.OrderBy(p => p.X).ToArray(), Tention);
+            e.Graphics.DrawPath(Pens.Red, Path2);
+        }
+
+        private void Panel2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                var thePoint = (PointShow)listBox2.SelectedItem;
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                Points2.Remove(thePoint.point);
+            }
+            Points2.Add(e.Location);
+            listBox2.Items.Add(new PointShow() { point = e.Location });
+            listBox2.Refresh();
+            panel2.Refresh();
+            ColorLabels();
+        }
+
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Draw the points.
+            foreach (Point point in Points3)
+                e.Graphics.FillEllipse(Brushes.Black,
+                    point.X - 3, point.Y - 3, 5, 5);
+            if (Points3.Count < 2) return;
+
+
+            // Draw the curve.
+            Path3 = new GraphicsPath();
+            Path3.AddCurve(Points3.OrderBy(p => p.X).ToArray(), Tention);
+            e.Graphics.DrawPath(Pens.Red, Path3);
+        }
+
+        private void Panel3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox3.SelectedItem != null)
+            {
+                var thePoint = (PointShow)listBox3.SelectedItem;
+                listBox3.Items.Remove(listBox3.SelectedItem);
+                Points3.Remove(thePoint.point);
+            }
+            Points3.Add(e.Location);
+            listBox3.Items.Add(new PointShow() { point = e.Location });
+            listBox3.Refresh();
+            panel3.Refresh();
+            ColorLabels();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Tention = float.Parse(textBox1.Text);
+        }
+    }
+}
